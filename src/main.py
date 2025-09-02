b4218f9df206570bf2aafa0ccf8b537ec214c9c0
Chanuka Dilshan <mchanuka72@gmail.com>
2025-09-02 17:42:38 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""
+Entry script for your ML project.
+"""
+
+def __main__():
+    print("🚀 Project is ready. Start building in src/ !")
+    print("Tip: open ML_Workflow.txt for a step-by-step checklist.")
+
+if __name__ == "__main__":
+    __main__()
